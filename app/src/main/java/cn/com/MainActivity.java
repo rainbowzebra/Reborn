e8fd16d7b044641223b78505c8c0721c62029435
lf <lfdfhl@gmail.com>
2017-08-06 21:38:16 +0800

    提交修改后的代码

--- app/src/main/java/cn/com/MainActivity.java
+++ app/src/main/java/cn/com/MainActivity.java
@@ -3,6 +3,10 @@ package cn.com;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 原创作者：谷哥的小弟
+ * 博客地址：http://blog.csdn.net/lfdfhl
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
